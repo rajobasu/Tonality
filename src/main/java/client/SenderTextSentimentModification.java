a672e94c388faded8ea77eb673f9fa64e2d10e55
Shivang211 <77135750+Shivang211@users.noreply.github.com>
2021-07-24 04:40:41 +0530

    Update String Commit

--- src/main/java/client/SenderTextSentimentModification.java
+++ src/main/java/client/SenderTextSentimentModification.java
@@ -5,7 +5,8 @@ public class SenderTextSentimentModification {
   public static String updateString(String text) {
     String newText = text.replaceAll("what you doing", "wyd").replaceAll("what are you doing", "wyd")
     .replaceAll("how about you", "hbu").replaceAll("on my way", "OMW").replaceFirst("Hello", "Hey!")
-    .replaceAll(".", "\n");
+    .replaceAll(".", "\n").replaceFirst("?", "...?").replaceAll("How are you doing?", "What's Up!!")
+    .replaceFirst("I don't know", "idk");
     return newText;
   }
 
